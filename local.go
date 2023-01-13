81c994b1fd9fd635fa03adaeec4a0fac28349821
yanagitani <takanori.yanagitani@gmail.com>
2023-01-13 09:10:28 +0900

    docs: more docs

--- local.go
+++ local.go
@@ -1,5 +1,9 @@
 package local
 
+// LocalFilterNew creates a new closure which returns only required values.
+//
+// # Arguments
+//   - f: The closure which checks if a value required or not.
 func LocalFilterNew[V, F any](f func(V, F) (keep bool)) func(all []V, filter F) []V {
 	return func(all []V, filter F) (filtered []V) {
 		for _, val := range all {
@@ -12,8 +16,10 @@ func LocalFilterNew[V, F any](f func(V, F) (keep bool)) func(all []V, filter F) 
 	}
 }
 
+// LocalFilter is a closure which checks if a value must be required or not.
 type LocalFilter[V, F any] func(value V, filter F) (keep bool)
 
+// And creates a new local filter which uses two closures to check a value.
 func (l LocalFilter[V, F]) And(other LocalFilter[V, F]) LocalFilter[V, F] {
 	return func(val V, flt F) (keep bool) {
 		var a bool = l(val, flt)
