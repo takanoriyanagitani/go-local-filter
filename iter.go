571a888bf14b2b4c282746f20e5da1b4e75010ca
yanagitani <takanori.yanagitani@gmail.com>
2023-01-13 09:40:58 +0900

    feat: gets required unpacked values from an iterator.

--- iter.go
+++ iter.go
@@ -41,3 +41,55 @@ func Iter2UnpackedNew[I, P, U any](
 		return
 	}
 }
+
+// Iter2UnpackedWithFilterNew creates a new closure which gets required unpacked items.
+//
+// # Arguments
+//   - packed2unpacked: Gets an unpacked item from a packed item.
+//   - hasNext: Checks if an iterator has a next item.
+//   - getPacked: Gets a packed item from an iterator.
+//   - getError: Gets an error if exists.
+//   - coarseFilter: Checks if a packed value is required or not.
+//   - fineFilter: Checks if an unpacked value is required or not.
+func Iter2UnpackedWithFilterNew[I, P, U, F any](
+	packed2unpacked func(packed *P) (unpacked U, e error),
+	hasNext func(iter I) bool,
+	getPacked func(iter I, buf *P) error,
+	getError func(iter I) error,
+	coarseFilter func(packed *P, filter *F) (keep bool),
+	fineFilter func(unpacked *U, filter *F) (keep bool),
+) func(ctx context.Context, iter I, filter *F) (unpacked []U, e error) {
+	return func(ctx context.Context, iter I, filter *F) (unpacked []U, e error) {
+		var buf P
+		for hasNext(iter) {
+			e = getPacked(iter, &buf)
+			if nil != e {
+				return nil, e
+			}
+
+			var keepCoarse bool = coarseFilter(&buf, filter)
+			if !keepCoarse {
+				continue
+			}
+
+			unpackedItem, e := packed2unpacked(&buf)
+			if nil != e {
+				return nil, e
+			}
+
+			var keepFine bool = fineFilter(&unpackedItem, filter)
+			if !keepFine {
+				continue
+			}
+
+			unpacked = append(unpacked, unpackedItem)
+		}
+
+		e = getError(iter)
+		if nil != e {
+			return nil, e
+		}
+
+		return
+	}
+}
--- iter_test.go
+++ iter_test.go
@@ -17,6 +17,11 @@ type testIterUnpacked struct {
 	bloom     [2]uint64
 }
 
+type testIterFilter struct {
+	key    uint8
+	bloom1 uint64
+}
+
 func TestIter(t *testing.T) {
 	t.Parallel()
 
@@ -109,6 +114,297 @@ func TestIter(t *testing.T) {
 
 			t.Run("no error", assertNil(e))
 			t.Run("single item", assertEq(len(unpacked), 1))
+
+			var unpackedItem testIterUnpacked = unpacked[0]
+			t.Run("sub bucket match", assertEq(unpackedItem.subBucket, 0x3776))
+			t.Run("rowid match", assertEq(unpackedItem.rowId, 0x01234567))
+			t.Run("bloom 0 match", assertEq(unpackedItem.bloom[0], 0x0123456789abcdef))
+			t.Run("bloom 1 match", assertEq(unpackedItem.bloom[1], 0x0123456789abcdef))
+		})
+	})
+
+	t.Run("Iter2UnpackedWithFilterNew", func(t *testing.T) {
+		t.Parallel()
+
+		t.Run("empty", func(t *testing.T) {
+			t.Parallel()
+
+			var dummyIter uint8 = 0
+			hasNext := func(iter *uint8) (foundNext bool) {
+				return false
+			}
+			getPacked := func(_iter *uint8, buf *testIterPacked) error {
+				buf.key = 0x37
+				buf.val[0] = 0x76
+
+				buf.val[1] = 0x01
+				buf.val[2] = 0x23
+				buf.val[3] = 0x45
+				buf.val[4] = 0x67
+
+				buf.val[0x5] = 0x01
+				buf.val[0x6] = 0x23
+				buf.val[0x7] = 0x45
+				buf.val[0x8] = 0x67
+				buf.val[0x9] = 0x89
+				buf.val[0xa] = 0xab
+				buf.val[0xb] = 0xcd
+				buf.val[0xc] = 0xef
+
+				buf.val[0x0d] = 0x01
+				buf.val[0x0e] = 0x23
+				buf.val[0x0f] = 0x45
+				buf.val[0x10] = 0x67
+				buf.val[0x11] = 0x89
+				buf.val[0x12] = 0xab
+				buf.val[0x13] = 0xcd
+				buf.val[0x14] = 0xef
+
+				return nil
+			}
+			getError := func(_iter *uint8) error { return nil }
+			packed2unpacked := func(p *testIterPacked) (u testIterUnpacked, e error) {
+				u.subBucket = (uint16(p.key) << 8) | uint16(p.val[0])
+				u.rowId = int32(binary.BigEndian.Uint32(p.val[1:]))
+				u.bloom[0] = binary.BigEndian.Uint64(p.val[0x5:])
+				u.bloom[1] = binary.BigEndian.Uint64(p.val[0xd:])
+				return
+			}
+
+			coarseFilter := func(p *testIterPacked, _f *testIterFilter) (keep bool) { return true }
+			fineFilter := func(u *testIterUnpacked, _f *testIterFilter) (keep bool) { return true }
+
+			var iter2unpacked func(
+				ctx context.Context,
+				iter *uint8,
+				f *testIterFilter,
+			) (unpacked []testIterUnpacked, e error) = Iter2UnpackedWithFilterNew(
+				packed2unpacked,
+				hasNext,
+				getPacked,
+				getError,
+				coarseFilter,
+				fineFilter,
+			)
+
+			unpacked, e := iter2unpacked(context.Background(), &dummyIter, nil)
+
+			t.Run("no error", assertNil(e))
+			t.Run("no items", assertEq(len(unpacked), 0))
+		})
+
+		t.Run("coarse filter", func(t *testing.T) {
+			t.Parallel()
+
+			var dummyIter uint8 = 0
+			hasNext := func(iter *uint8) (foundNext bool) {
+				foundNext = 0 == *iter
+				*iter += 1
+				return
+			}
+			getPacked := func(_iter *uint8, buf *testIterPacked) error {
+				buf.key = 0x37
+				buf.val[0] = 0x76
+
+				buf.val[1] = 0x01
+				buf.val[2] = 0x23
+				buf.val[3] = 0x45
+				buf.val[4] = 0x67
+
+				buf.val[0x5] = 0x01
+				buf.val[0x6] = 0x23
+				buf.val[0x7] = 0x45
+				buf.val[0x8] = 0x67
+				buf.val[0x9] = 0x89
+				buf.val[0xa] = 0xab
+				buf.val[0xb] = 0xcd
+				buf.val[0xc] = 0xef
+
+				buf.val[0x0d] = 0x01
+				buf.val[0x0e] = 0x23
+				buf.val[0x0f] = 0x45
+				buf.val[0x10] = 0x67
+				buf.val[0x11] = 0x89
+				buf.val[0x12] = 0xab
+				buf.val[0x13] = 0xcd
+				buf.val[0x14] = 0xef
+
+				return nil
+			}
+			getError := func(_iter *uint8) error { return nil }
+			packed2unpacked := func(p *testIterPacked) (u testIterUnpacked, e error) {
+				u.subBucket = (uint16(p.key) << 8) | uint16(p.val[0])
+				u.rowId = int32(binary.BigEndian.Uint32(p.val[1:]))
+				u.bloom[0] = binary.BigEndian.Uint64(p.val[0x5:])
+				u.bloom[1] = binary.BigEndian.Uint64(p.val[0xd:])
+				return
+			}
+
+			coarseFilter := func(p *testIterPacked, _f *testIterFilter) (keep bool) { return false }
+			fineFilter := func(u *testIterUnpacked, _f *testIterFilter) (keep bool) { return true }
+
+			var iter2unpacked func(
+				ctx context.Context,
+				iter *uint8,
+				f *testIterFilter,
+			) (unpacked []testIterUnpacked, e error) = Iter2UnpackedWithFilterNew(
+				packed2unpacked,
+				hasNext,
+				getPacked,
+				getError,
+				coarseFilter,
+				fineFilter,
+			)
+
+			unpacked, e := iter2unpacked(context.Background(), &dummyIter, nil)
+
+			t.Run("no error", assertNil(e))
+			t.Run("no items", assertEq(len(unpacked), 0))
+		})
+
+		t.Run("fine filter", func(t *testing.T) {
+			t.Parallel()
+
+			var dummyIter uint8 = 0
+			hasNext := func(iter *uint8) (foundNext bool) {
+				foundNext = 0 == *iter
+				*iter += 1
+				return
+			}
+			getPacked := func(_iter *uint8, buf *testIterPacked) error {
+				buf.key = 0x37
+				buf.val[0] = 0x76
+
+				buf.val[1] = 0x01
+				buf.val[2] = 0x23
+				buf.val[3] = 0x45
+				buf.val[4] = 0x67
+
+				buf.val[0x5] = 0x01
+				buf.val[0x6] = 0x23
+				buf.val[0x7] = 0x45
+				buf.val[0x8] = 0x67
+				buf.val[0x9] = 0x89
+				buf.val[0xa] = 0xab
+				buf.val[0xb] = 0xcd
+				buf.val[0xc] = 0xef
+
+				buf.val[0x0d] = 0x01
+				buf.val[0x0e] = 0x23
+				buf.val[0x0f] = 0x45
+				buf.val[0x10] = 0x67
+				buf.val[0x11] = 0x89
+				buf.val[0x12] = 0xab
+				buf.val[0x13] = 0xcd
+				buf.val[0x14] = 0xef
+
+				return nil
+			}
+			getError := func(_iter *uint8) error { return nil }
+			packed2unpacked := func(p *testIterPacked) (u testIterUnpacked, e error) {
+				u.subBucket = (uint16(p.key) << 8) | uint16(p.val[0])
+				u.rowId = int32(binary.BigEndian.Uint32(p.val[1:]))
+				u.bloom[0] = binary.BigEndian.Uint64(p.val[0x5:])
+				u.bloom[1] = binary.BigEndian.Uint64(p.val[0xd:])
+				return
+			}
+
+			coarseFilter := func(p *testIterPacked, _f *testIterFilter) (keep bool) { return true }
+			fineFilter := func(u *testIterUnpacked, _f *testIterFilter) (keep bool) { return false }
+
+			var iter2unpacked func(
+				ctx context.Context,
+				iter *uint8,
+				f *testIterFilter,
+			) (unpacked []testIterUnpacked, e error) = Iter2UnpackedWithFilterNew(
+				packed2unpacked,
+				hasNext,
+				getPacked,
+				getError,
+				coarseFilter,
+				fineFilter,
+			)
+
+			unpacked, e := iter2unpacked(context.Background(), &dummyIter, nil)
+
+			t.Run("no error", assertNil(e))
+			t.Run("no items", assertEq(len(unpacked), 0))
+		})
+
+		t.Run("single item", func(t *testing.T) {
+			t.Parallel()
+
+			var dummyIter uint8 = 0
+			hasNext := func(iter *uint8) (foundNext bool) {
+				foundNext = 0 == *iter
+				*iter += 1
+				return
+			}
+			getPacked := func(_iter *uint8, buf *testIterPacked) error {
+				buf.key = 0x37
+				buf.val[0] = 0x76
+
+				buf.val[1] = 0x01
+				buf.val[2] = 0x23
+				buf.val[3] = 0x45
+				buf.val[4] = 0x67
+
+				buf.val[0x5] = 0x01
+				buf.val[0x6] = 0x23
+				buf.val[0x7] = 0x45
+				buf.val[0x8] = 0x67
+				buf.val[0x9] = 0x89
+				buf.val[0xa] = 0xab
+				buf.val[0xb] = 0xcd
+				buf.val[0xc] = 0xef
+
+				buf.val[0x0d] = 0x01
+				buf.val[0x0e] = 0x23
+				buf.val[0x0f] = 0x45
+				buf.val[0x10] = 0x67
+				buf.val[0x11] = 0x89
+				buf.val[0x12] = 0xab
+				buf.val[0x13] = 0xcd
+				buf.val[0x14] = 0xef
+
+				return nil
+			}
+			getError := func(_iter *uint8) error { return nil }
+			packed2unpacked := func(p *testIterPacked) (u testIterUnpacked, e error) {
+				u.subBucket = (uint16(p.key) << 8) | uint16(p.val[0])
+				u.rowId = int32(binary.BigEndian.Uint32(p.val[1:]))
+				u.bloom[0] = binary.BigEndian.Uint64(p.val[0x5:])
+				u.bloom[1] = binary.BigEndian.Uint64(p.val[0xd:])
+				return
+			}
+
+			coarseFilter := func(p *testIterPacked, f *testIterFilter) (keep bool) {
+				return p.key == f.key
+			}
+			fineFilter := func(u *testIterUnpacked, f *testIterFilter) (keep bool) {
+				return (u.bloom[1] & f.bloom1) == f.bloom1
+			}
+
+			var iter2unpacked func(
+				ctx context.Context,
+				iter *uint8,
+				f *testIterFilter,
+			) (unpacked []testIterUnpacked, e error) = Iter2UnpackedWithFilterNew(
+				packed2unpacked,
+				hasNext,
+				getPacked,
+				getError,
+				coarseFilter,
+				fineFilter,
+			)
+
+			unpacked, e := iter2unpacked(context.Background(), &dummyIter, &testIterFilter{
+				key:    0x37,
+				bloom1: 0x00000000000000ef,
+			})
+
+			t.Run("no error", assertNil(e))
+			t.Run("no items", assertEq(len(unpacked), 1))
 
 			var unpackedItem testIterUnpacked = unpacked[0]
 			t.Run("sub bucket match", assertEq(unpackedItem.subBucket, 0x3776))
