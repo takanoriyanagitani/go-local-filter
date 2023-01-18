0c55d401fd6417cf13fe892d1f9c70247dfa209c
yanagitani <takanori.yanagitani@gmail.com>
2023-01-19 07:36:58 +0900

    docs: more docs

--- bucket.go
+++ bucket.go
@@ -1,11 +1,13 @@
 package local
 
-type Bucket struct {
-	name string
-}
+// Bucket contains a bucket name.
+type Bucket struct{ name string }
 
-func BucketNew(name string) Bucket {
-	return Bucket{name}
-}
+// BucketNew creates a bucket from a checked name.
+//
+// # Arguments
+//   - name: A checked name(this method has no validation logic).
+func BucketNew(name string) Bucket { return Bucket{name} }
 
+// AsString returns a raw bucket string name.
 func (b Bucket) AsString() string { return b.name }
