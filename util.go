4942eabd068127887efd82ef8e6ecf5757926576
yanagitani <takanori.yanagitani@gmail.com>
2023-01-20 16:46:51 +0900

    feat: err func creator

--- util.go
+++ util.go
@@ -6,3 +6,7 @@ func callEither[T, U any](callA bool, a, b func(T) (U, error)) func(T) (U, error
 	}
 	return b
 }
+
+func errFuncNew[T, U any](noerr func(T) U) func(T) (U, error) {
+	return func(t T) (U, error) { return noerr(t), nil }
+}
