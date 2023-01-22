531c94810fa42f816e2cc9900c72d343b495fd63
yanagitani <takanori.yanagitani@gmail.com>
2023-01-22 09:31:37 +0900

    fix: missing function

--- util.go
+++ util.go
@@ -10,3 +10,12 @@ func callEither[T, U any](callA bool, a, b func(T) (U, error)) func(T) (U, error
 func errFuncNew[T, U any](noerr func(T) U) func(T) (U, error) {
 	return func(t T) (U, error) { return noerr(t), nil }
 }
+
+func doEither(doFormer bool, former func() error, latter func() error) error {
+	switch doFormer {
+	case true:
+		return former()
+	default:
+		return latter()
+	}
+}
